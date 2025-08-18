4bb1a0fa75ac4a8983abecf6278165d85445ab13
Saif <ro1c@yahoo.com>
2025-08-18 09:22:38 +0300

    feat: enable users to sign up

--- src/components/sign-up-form.tsx
+++ src/components/sign-up-form.tsx
@@ -1,73 +1,94 @@
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
+import { useAuth } from "@/context/auth-context";
 
 export function SignUpForm({
-    className,
-    ...props
+  className,
+  ...props
 }: React.ComponentProps<"div">) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const { signUp } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-    const handleSignUp = () => {};
+  const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
+    try {
+      await signUp(email, password);
+    } catch (error) {
+      setError(String(error));
+    }
 
-    return (
-        <div className={cn("flex flex-col gap-6", className)} {...props}>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Create a new account</CardTitle>
-                    <CardDescription>
-                        Enter your email below to create a new account
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <form>
-                        <div className="flex flex-col gap-6">
-                            <div className="grid gap-3">
-                                <Label htmlFor="email">Email</Label>
-                                <Input
-                                    id="email"
-                                    type="email"
-                                    placeholder="m@example.com"
-                                    required
-                                />
-                            </div>
-                            <div className="grid gap-3">
-                                <div className="flex items-center">
-                                    <Label htmlFor="password">Password</Label>
-                                </div>
-                                <Input id="password" type="password" required />
-                            </div>
-                            <div className="flex flex-col gap-3">
-                                <Button type="submit" className="w-full">
-                                    Sign Up
-                                </Button>
-                                <Button variant="outline" className="w-full">
-                                    Sign Up with Google
-                                </Button>
-                            </div>
-                        </div>
-                        <div className="mt-4 text-center text-sm">
-                            Already have an account?{" "}
-                            <a
-                                href="/login"
-                                className="underline underline-offset-4"
-                            >
-                                Sign in
-                            </a>
-                        </div>
-                    </form>
-                </CardContent>
-            </Card>
-        </div>
-    );
+    setLoading(false);
+  };
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Create a new account</CardTitle>
+          <CardDescription>
+            Enter your email below to create a new account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSignUp}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <p className="text-center text-red-400">{error}</p>
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  {isLoading ? "Signing up..." : "Sign Up"}
+                </Button>
+                <Button variant="outline" className="w-full">
+                  Sign Up with Google
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <a href="/login" className="underline underline-offset-4">
+                Sign in
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
--- src/pages/signup-page.tsx
+++ src/pages/signup-page.tsx
@@ -1,11 +1,21 @@
 import { SignUpForm } from "@/components/sign-up-form";
+import { useAuth } from "@/context/auth-context";
+import { Navigate, useLocation } from "react-router-dom";
 
 export default function SignUpPage() {
-    return (
-        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-            <div className="w-full max-w-sm">
-                <SignUpForm />
-            </div>
-        </div>
-    );
+  const { isAuth } = useAuth();
+  const location = useLocation();
+  //console.log(location);
+  if (isAuth) {
+    const from = location.state?.from.pathname;
+    if (from) return <Navigate to={from} />;
+    else return <Navigate to="/notes" />;
+  }
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <SignUpForm />
+      </div>
+    </div>
+  );
 }
